3824d55328250931ae47a2afad391a2e3ec11c16
lee <charismafight@hotmail.com>
2017-12-19 16:58:37 +0800

    change the cookies to session

--- tangowithdjango/rango/views.py
+++ tangowithdjango/rango/views.py
@@ -16,23 +16,26 @@ def index(request):
     pages = Page.objects.order_by('-views')[:5]
     context = {'categories': qs, 'pages': pages}
 
-    visits = int(request.COOKIES.get('visits', 1))
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
     reset_last_visit_time = False
+    last_visit = request.session.get('last_visit')
 
-    if 'last_visit' in request.COOKIES:
-        last_visit = request.COOKIES['last_visit']
+    if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-        if (datetime.now() - last_visit_time).seconds > 5:
+        if (datetime.now() - last_visit_time).seconds > 0:
             visits += 1
             reset_last_visit_time = True
     else:
         reset_last_visit_time = True
 
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+
     context['visits'] = visits
     response = render(request, 'rango/index.html', context)
-    if reset_last_visit_time:
-        response.set_cookie('last_visit', datetime.now())
-        response.set_cookie('visits', visits)
     return response
 
 
